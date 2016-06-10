d9c95940d704a34ce7ed80e56a3889a82b07df04
iarenasbeyond <iarenas@bebeyond.es>
2016-06-10 13:59:28 +0000

    Add files via upload

--- components/com_ponente/models/albums.php
+++ components/com_ponente/models/albums.php
@@ -18,29 +18,29 @@ jimport('joomla.application.component.modellist');
  */
 class PonenteModelAlbums extends JModelList
 {
-	/**
-	 * Constructor.
-	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @see        JController
-	 * @since      1.6
-	 */
+/**
+	* Constructor.
+	*
+	* @param   array  $config  An optional associative array of configuration settings.
+	*
+	* @see        JController
+	* @since      1.6
+	*/
 	public function __construct($config = array())
 	{
 		if (empty($config['filter_fields']))
 		{
 			$config['filter_fields'] = array(
-				'id', 'a.id',
-				'ordering', 'a.ordering',
-				'state', 'a.state',
-				'created_by', 'a.created_by',
-				'modified_by', 'a.modified_by',
-				'grupo', 'a.grupo',
-				'nombre', 'a.nombre',
-				'fecha', 'a.fecha',
-				'descripcion', 'a.descripcion',
-				'imagen', 'a.imagen',
+				'id', 'a.`id`',
+				'ordering', 'a.`ordering`',
+				'state', 'a.`state`',
+				'created_by', 'a.`created_by`',
+				'modified_by', 'a.`modified_by`',
+				'grupo', 'a.`grupo`',
+				'nombre', 'a.`nombre`',
+				'fecha', 'a.`fecha`',
+				'descripcion', 'a.`descripcion`',
+				'imagen', 'a.`imagen`',
 			);
 		}
 
@@ -58,134 +58,95 @@ class PonenteModelAlbums extends JModelList
 	 * @return void
 	 *
 	 * @throws Exception
-	 *
-	 * @since    1.6
 	 */
 	protected function populateState($ordering = null, $direction = null)
 	{
 		// Initialise variables.
-		$app = JFactory::getApplication();
+		$app = JFactory::getApplication('administrator');
 
-		// List state information
-		$limit = $app->getUserStateFromRequest('global.list.limit', 'limit', $app->get('list_limit'));
-		$this->setState('list.limit', $limit);
+		// Load the filter state.
+		$search = $app->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
 
-		$limitstart = $app->getUserStateFromRequest('limitstart', 'limitstart', 0);
-		$this->setState('list.start', $limitstart);
+		$published = $app->getUserStateFromRequest($this->context . '.filter.state', 'filter_published', '', 'string');
+		$this->setState('filter.state', $published);
+		// Filtering grupo
+		$this->setState('filter.grupo', $app->getUserStateFromRequest($this->context.'.filter.grupo', 'filter_grupo', '', 'string'));
 
-		if ($list = $app->getUserStateFromRequest($this->context . '.list', 'list', array(), 'array'))
-		{
-			foreach ($list as $name => $value)
-			{
-				// Extra validations
-				switch ($name)
-				{
-					case 'fullordering':
-						$orderingParts = explode(' ', $value);
+		// Filtering fecha
+		$this->setState('filter.fecha.from', $app->getUserStateFromRequest($this->context.'.filter.fecha.from', 'filter_from_fecha', '', 'string'));
+		$this->setState('filter.fecha.to', $app->getUserStateFromRequest($this->context.'.filter.fecha.to', 'filter_to_fecha', '', 'string'));
 
-						if (count($orderingParts) >= 2)
-						{
-							// Latest part will be considered the direction
-							$fullDirection = end($orderingParts);
 
-							if (in_array(strtoupper($fullDirection), array('ASC', 'DESC', '')))
-							{
-								$this->setState('list.direction', $fullDirection);
-							}
+		// Load the parameters.
+		$params = JComponentHelper::getParams('com_ponente');
+		$this->setState('params', $params);
 
-							unset($orderingParts[count($orderingParts) - 1]);
+		// List state information.
+		parent::populateState('a.grupo', 'asc');
+	}
 
-							// The rest will be the ordering
-							$fullOrdering = implode(' ', $orderingParts);
 
-							if (in_array($fullOrdering, $this->filter_fields))
-							{
-								$this->setState('list.ordering', $fullOrdering);
-							}
-						}
-						else
-						{
-							$this->setState('list.ordering', $ordering);
-							$this->setState('list.direction', $direction);
-						}
-						break;
+	public function getAlbumsByGroup($pk)
+	{
 
-					case 'ordering':
-						if (!in_array($value, $this->filter_fields))
-						{
-							$value = $ordering;
-						}
-						break;
 
-					case 'direction':
-						if (!in_array(strtoupper($value), array('ASC', 'DESC', '')))
-						{
-							$value = $direction;
-						}
-						break;
 
-					case 'limit':
-						$limit = $value;
-						break;
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true);
 
-					// Just to keep the default case
-					default:
-						$value = $value;
-						break;
-				}
+		$query
+			->select(
+				$db->quoteName(array('id', 'fecha', 'nombre', 'descripcion', 'imagen'))
+			)
+			->from($db->quoteName('#__ponente_album', 'a'))
+			->where($db->quoteName('grupo') . ' = ' . (int) $pk);
 
-				$this->setState('list.' . $name, $value);
-			}
-		}
+		$db->setQuery($query);
 
-		// Receive & set filters
-		if ($filters = $app->getUserStateFromRequest($this->context . '.filter', 'filter', array(), 'array'))
-		{
-			foreach ($filters as $name => $value)
-			{
-				$this->setState('filter.' . $name, $value);
-			}
-		}
+		return $db->loadObjectList();
 
-		$ordering = $app->input->get('filter_order');
+	}
 
-		if (!empty($ordering))
-		{
-			$list             = $app->getUserState($this->context . '.list');
-			$list['ordering'] = $app->input->get('filter_order');
-			$app->setUserState($this->context . '.list', $list);
-		}
+	public function getLastAlbum($pk)
+	{
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true);
 
-		$orderingDirection = $app->input->get('filter_order_Dir');
+		$query
+			->select(
+				$db->quoteName(array('id', 'fecha', 'nombre', 'descripcion', 'imagen'))
+			)
+			->from($db->quoteName('#__ponente_album', 'a'))
+			->where($db->quoteName('grupo') . ' = ' . (int) $pk)
+			->order ('a.fecha DESC')
+			->setlimit ('1');
 
-		if (!empty($orderingDirection))
-		{
-			$list              = $app->getUserState($this->context . '.list');
-			$list['direction'] = $app->input->get('filter_order_Dir');
-			$app->setUserState($this->context . '.list', $list);
-		}
+		$db->setQuery($query);
 
-		$list = $app->getUserState($this->context . '.list');
+		return $db->loadObjectList();
+	}
 
-		if (empty($list['ordering']))
-{
-	$list['ordering'] = 'ordering';
-}
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param   string  $id  A prefix for the store id.
+	 *
+	 * @return   string A store id.
+	 *
+	 * @since    1.6
+	 */
+	protected function getStoreId($id = '')
+	{
+		// Compile the store id.
+		$id .= ':' . $this->getState('filter.search');
+		$id .= ':' . $this->getState('filter.state');
 
-if (empty($list['direction']))
-{
-	$list['direction'] = 'asc';
-}
-
-		if (isset($list['ordering']))
-		{
-			$this->setState('list.ordering', $list['ordering']);
-		}
-
-		if (isset($list['direction']))
-		{
-			$this->setState('list.direction', $list['direction']);
-		}
+		return parent::getStoreId($id);
 	}
 
 	/**
@@ -202,31 +163,38 @@ if (empty($list['direction']))
 		$query = $db->getQuery(true);
 
 		// Select the required fields from the table.
-		$query
-			->select(
-				$this->getState(
-					'list.select', 'DISTINCT a.*'
-				)
-			);
-
+		$query->select(
+			$this->getState(
+				'list.select', 'DISTINCT a.*'
+			)
+		);
 		$query->from('`#__ponente_album` AS a');
-		
-		// Join over the users for the checked out user.
-		$query->select('uc.name AS editor');
-		$query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
 
-		// Join over the created by field 'created_by'
-		$query->join('LEFT', '#__users AS created_by ON created_by.id = a.created_by');
+		// Join over the users for the checked out user
+		$query->select("uc.name AS editor");
+		$query->join("LEFT", "#__users AS uc ON uc.id=a.checked_out");
 
-		// Join over the created by field 'modified_by'
-		$query->join('LEFT', '#__users AS modified_by ON modified_by.id = a.modified_by');
+		// Join over the user field 'created_by'
+		$query->select('`created_by`.name AS `created_by`');
+		$query->join('LEFT', '#__users AS `created_by` ON `created_by`.id = a.`created_by`');
+
+		// Join over the user field 'modified_by'
+		$query->select('`modified_by`.name AS `modified_by`');
+		$query->join('LEFT', '#__users AS `modified_by` ON `modified_by`.id = a.`modified_by`');
 		// Join over the foreign key 'grupo'
 		$query->select('`#__ponente_grupo_2388112`.`nombre` AS grupos_fk_value_2388112');
 		$query->join('LEFT', '#__ponente_grupo AS #__ponente_grupo_2388112 ON #__ponente_grupo_2388112.`id` = a.`grupo`');
-		
-		if (!JFactory::getUser()->authorise('core.edit', 'com_ponente'))
+
+		// Filter by published state
+		$published = $this->getState('filter.state');
+
+		if (is_numeric($published))
 		{
-			$query->where('a.state = 1');
+			$query->where('a.state = ' . (int) $published);
+		}
+		elseif ($published === '')
+		{
+			$query->where('(a.state IN (0, 1))');
 		}
 
 		// Filter by search in title
@@ -241,36 +209,27 @@ if (empty($list['direction']))
 			else
 			{
 				$search = $db->Quote('%' . $db->escape($search, true) . '%');
-				$query->where('(#__ponente_grupo_2388112.nombre LIKE ' . $search . '  OR  a.nombre LIKE ' . $search . ' )');
+				$query->where('(#__ponente_grupo_2388112.nombre LIKE ' . $search . '  OR  a.nombre LIKE ' . $search . '  OR  a.fecha LIKE ' . $search . ' )');
 			}
 		}
-		
 
-		// Filtering grupo
+
+		//Filtering grupo
 		$filter_grupo = $this->state->get("filter.grupo");
 		if ($filter_grupo)
 		{
-			$query->where("a.grupo = '".$db->escape($filter_grupo)."'");
+			$query->where("a.`grupo` = '".$db->escape($filter_grupo)."'");
 		}
 
-		// Filtering fecha
-		// Checking "_dateformat"
-		$filter_fecha_from = $this->state->get("filter.fecha_from_dateformat");
-		$filter_Qfecha_from = (!empty($filter_fecha_from)) ? $this->isValidDate($filter_fecha_from) : null;
-
-		if ($filter_Qfecha_from != null)
-		{
-			$query->where("a.fecha >= '" . $db->escape($filter_Qfecha_from) . "'");
+		//Filtering fecha
+		$filter_fecha_from = $this->state->get("filter.fecha.from");
+		if ($filter_fecha_from) {
+			$query->where("a.`fecha` >= '".$db->escape($filter_fecha_from)."'");
 		}
-
-		$filter_fecha_to = $this->state->get("filter.fecha_to_dateformat");
-		$filter_Qfecha_to = (!empty($filter_fecha_to)) ? $this->isValidDate($filter_fecha_to) : null ;
-
-		if ($filter_Qfecha_to != null)
-		{
-			$query->where("a.fecha <= '" . $db->escape($filter_Qfecha_to) . "'");
+		$filter_fecha_to = $this->state->get("filter.fecha.to");
+		if ($filter_fecha_to) {
+			$query->where("a.`fecha` <= '".$db->escape($filter_fecha_to)."'");
 		}
-
 		// Add the list ordering clause.
 		$orderCol  = $this->state->get('list.ordering');
 		$orderDirn = $this->state->get('list.direction');
@@ -284,91 +243,39 @@ if (empty($list['direction']))
 	}
 
 	/**
-	 * Method to get an array of data items
+	 * Get an array of data items
 	 *
-	 * @return  mixed An array of data on success, false on failure.
+	 * @return mixed Array of data items on success, false on failure.
 	 */
 	public function getItems()
 	{
 		$items = parent::getItems();
-		
-		foreach ($items as $item)
-		{
-			if (isset($item->grupo) && $item->grupo != '')
+
+		foreach ($items as $oneItem) {
+
+			if (isset($oneItem->grupo))
 			{
-				if (is_object($item->grupo))
-				{
-					$item->grupo = \Joomla\Utilities\ArrayHelper::fromObject($item->grupo);
-				}
+				$values = explode(',', $oneItem->grupo);
 
-				$values = (is_array($item->grupo)) ? $item->grupo : explode(',', $item->grupo);
 				$textValue = array();
-
-				foreach ($values as $value)
-				{
+				foreach ($values as $value){
 					$db = JFactory::getDbo();
 					$query = $db->getQuery(true);
 					$query
 							->select('`nombre`')
-						->from($db->quoteName('#__ponente_grupo'))
-						->where($db->quoteName('id') . ' = ' . $db->quote($db->escape($value)));
+							->from('`#__ponente_grupo`')
+							->where($db->quoteName('id') . ' = '. $db->quote($db->escape($value)));
 					$db->setQuery($query);
 					$results = $db->loadObject();
-
-					if ($results)
-					{
+					if ($results) {
 						$textValue[] = $results->nombre;
 					}
 				}
 
-				$item->grupo = !empty($textValue) ? implode(', ', $textValue) : $item->grupo;
+			$oneItem->grupo = !empty($textValue) ? implode(', ', $textValue) : $oneItem->grupo;
+
 			}
-
 		}
-
 		return $items;
-	}
-
-	/**
-	 * Overrides the default function to check Date fields format, identified by
-	 * "_dateformat" suffix, and erases the field if it's not correct.
-	 *
-	 * @return void
-	 */
-	protected function loadFormData()
-	{
-		$app              = JFactory::getApplication();
-		$filters          = $app->getUserState($this->context . '.filter', array());
-		$error_dateformat = false;
-
-		foreach ($filters as $key => $value)
-		{
-			if (strpos($key, '_dateformat') && !empty($value) && $this->isValidDate($value) == null)
-			{
-				$filters[$key]    = '';
-				$error_dateformat = true;
-			}
-		}
-
-		if ($error_dateformat)
-		{
-			$app->enqueueMessage(JText::_("COM_PONENTE_SEARCH_FILTER_DATE_FORMAT"), "warning");
-			$app->setUserState($this->context . '.filter', $filters);
-		}
-
-		return parent::loadFormData();
-	}
-
-	/**
-	 * Checks if a given date is valid and in a specified format (YYYY-MM-DD)
-	 *
-	 * @param   string  $date  Date to be checked
-	 *
-	 * @return bool
-	 */
-	private function isValidDate($date)
-	{
-		$date = str_replace('/', '-', $date);
-		return (date_create($date)) ? JFactory::getDate($date)->format("Y-m-d") : null;
 	}
 }
